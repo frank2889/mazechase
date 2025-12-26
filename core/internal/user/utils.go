ce2272f1b880dd45e4772393676d8af9f298aca6
Frank Pirets <Frank@MacBook-Air-3.local>
2025-12-26 10:55:36 +0100

    fix: Remove Secure flag from cookie, add debug logging
    - Secure flag causes issues with DigitalOcean reverse proxy
    - SameSite=Lax provides sufficient security
    - Added debug logging for cookie setting
    - Cookie name is now mc_auth (non-reserved)
    - Rebuilt frontend

--- core/internal/user/utils.go
+++ core/internal/user/utils.go
@@ -1,72 +1,72 @@
 package user
 
 import (
-	"crypto/rand"
-	"crypto/sha256"
-	"fmt"
-	"math/big"
-	"net/http"
+"crypto/rand"
+"crypto/sha256"
+"fmt"
+"math/big"
+"net/http"
 
-	"connectrpc.com/connect"
-	v1 "github.com/frank2889/mazechase/generated/auth/v1"
-	"github.com/rs/zerolog/log"
-	"golang.org/x/crypto/bcrypt"
+"connectrpc.com/connect"
+v1 "github.com/frank2889/mazechase/generated/auth/v1"
+"github.com/rs/zerolog/log"
+"golang.org/x/crypto/bcrypt"
 )
 
 func setCookie(user *User, response *connect.Response[v1.UserResponse]) {
-	cookie := http.Cookie{
-		Name:     AuthHeaderKey,
-		Value:    user.Token,
-		Path:     "/",
-		HttpOnly: true,
-		SameSite: http.SameSiteLaxMode,
-		Secure:   true, // Required for HTTPS (DigitalOcean)
-		MaxAge:   86400 * 30, // 30 days
-	}
+cookie := http.Cookie{
+Name:     AuthHeaderKey,
+Value:    user.Token,
+Path:     "/",
+HttpOnly: true,
+SameSite: http.SameSiteLaxMode,
+MaxAge:   86400 * 30, // 30 days
+}
 
-	response.Header().Add("Set-Cookie", cookie.String())
+log.Debug().Str("cookie", cookie.String()).Msg("Setting auth cookie")
+response.Header().Add("Set-Cookie", cookie.String())
 }
 
 func CreateAuthToken(length int) string {
-	const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-	var randomString []byte
+const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+var randomString []byte
 
-	for i := 0; i < length; i++ {
-		randomIndex, _ := rand.Int(rand.Reader, big.NewInt(int64(len(characters))))
-		randomString = append(randomString, characters[randomIndex.Int64()])
-	}
+for i := 0; i < length; i++ {
+randomIndex, _ := rand.Int(rand.Reader, big.NewInt(int64(len(characters))))
+randomString = append(randomString, characters[randomIndex.Int64()])
+}
 
-	return string(randomString)
+return string(randomString)
 }
 
 func checkPassword(inputPassword string, hashedPassword string) bool {
-	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(inputPassword))
-	if err != nil {
-		log.Error().Err(err).Msg("Failed Password check")
-		return false
-	}
-	return true
+err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(inputPassword))
+if err != nil {
+log.Error().Err(err).Msg("Failed Password check")
+return false
+}
+return true
 }
 
 func encryptPassword(password []byte) string {
-	hashedPassword, err := bcrypt.GenerateFromPassword(password, bcrypt.DefaultCost)
-	if err != nil {
-		panic(err)
-	}
-	return string(hashedPassword)
+hashedPassword, err := bcrypt.GenerateFromPassword(password, bcrypt.DefaultCost)
+if err != nil {
+panic(err)
+}
+return string(hashedPassword)
 }
 
 // EncryptPasswordPublic is a public wrapper for encryptPassword (used by database seeding)
 func EncryptPasswordPublic(password []byte) string {
-	return encryptPassword(password)
+return encryptPassword(password)
 }
 
 func hashString(input string) string {
-	hash := sha256.New()
-	hash.Write([]byte(input))
-	// Get the resulting hash sum
-	hashedBytes := hash.Sum(nil)
-	// Convert the hashed bytes to a hexadecimal string
-	hashedString := fmt.Sprintf("%x", hashedBytes)
-	return hashedString
+hash := sha256.New()
+hash.Write([]byte(input))
+// Get the resulting hash sum
+hashedBytes := hash.Sum(nil)
+// Convert the hashed bytes to a hexadecimal string
+hashedString := fmt.Sprintf("%x", hashedBytes)
+return hashedString
 }
